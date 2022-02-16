0aad6803fd031ffc5de00e17a271fc160b90e90a
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-16 23:49:08 +0000

    play completed

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',// IP address here,
-    port: 50541// PORT number here,
-  });
+const newClient = require('./client')
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
 
-  conn.on("data", (data) => {
-    console.log("you ded cuz you idled")
-  })
 
-  return conn;
-};
+newClient();
 
-console.log("Connecting ...");
-connect();
