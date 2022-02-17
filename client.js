ff7de7f933ec37b4d61cb5f090066b38a43a1d81
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-17 00:06:59 +0000

    most recently updated version

--- client.js
+++ client.js
@@ -4,16 +4,21 @@ const net = require("net");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',// IP address here,
-    port: 50541// PORT number here,
+    host: '165.227.47.243',
+    port: 50541
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("data", (data) => {
     console.log(data)
   })
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server")
+    conn.write("Name: BBB");
+  })
+  
 
   return conn;
 };
