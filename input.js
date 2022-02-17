81eee11221100b8203a64d881a04834a25217960
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-17 01:31:31 +0000

    imput completed

---
+++ input.js
@@ -0,0 +1,25 @@
+const { connect } = require("./client");
+const { setupInput } = require("./input");
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    process.exit();
+  };
+}
+
+console.log("Connecting ...");
+connect();
+setupInput();
+
+module.exports = setupInput;
